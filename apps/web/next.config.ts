49709d9a9347fe3e64158153b75ecf1d87565c24
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-07-28 21:15:51 +0200

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,10 @@
+import "@theseosaas/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+  output: "standalone",
+};
+
+export default nextConfig;
